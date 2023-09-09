8e1b70d1d733d53885c702d9d42f5e300e86fa39
ritalamykin <ritalamykin1@gmail.com>
2023-09-09 13:23:30 +0300

    commit

--- app.py
+++ app.py
@@ -37,19 +37,20 @@ app = Flask(__name__)
 def index():
     if request.method == "POST":
 
-        url = request.form["url"]
-        obj = FeatureExtraction(url)
-        x = np.array(obj.getFeaturesList()).reshape(1,30) 
+        # url = request.form["url"]
+        # obj = FeatureExtraction(url)
+        # x = np.array(obj.getFeaturesList()).reshape(1,30) 
 
-        y_pred =gbc.predict(x)[0]
-        #1 is safe       
-        #-1 is unsafe
-        y_pro_phishing = gbc.predict_proba(x)[0,0]
-        y_pro_non_phishing = gbc.predict_proba(x)[0,1]
-        # if(y_pred ==1 ):
-        pred = "It is {0:.2f} % safe to go ".format(y_pro_phishing*100)
-        #return render_template('index.html',xx =round(y_pro_non_phishing,2),url=url )
-        return str(y_pred)
+        # y_pred =gbc.predict(x)[0]
+        # #1 is safe       
+        # #-1 is unsafe
+        # y_pro_phishing = gbc.predict_proba(x)[0,0]
+        # y_pro_non_phishing = gbc.predict_proba(x)[0,1]
+        # # if(y_pred ==1 ):
+        # pred = "It is {0:.2f} % safe to go ".format(y_pro_phishing*100)
+        # #return render_template('index.html',xx =round(y_pro_non_phishing,2),url=url )
+        # return str(y_pred)
+        return "hello"
     #return render_template("index.html", xx =-1)
     return "none"
 
