dfed86e4eaed07be41d56498af6de41e29e2292e
Danielle Netser <netser1@mail.tau.ac.il>
2023-09-09 20:22:09 +0300

    commit

--- extension/content.js
+++ extension/content.js
@@ -18,29 +18,31 @@ async function markLinks() {
   var body = await response.json();
   } catch(err) {console.log(err)}
   console.log(body)
-  for (var i = 0; i < linksObjects.length; i++) {
-    var link = linksObjects[i];
-    if (linksStrings[i] in body){
-      // -1 refers to a malicous prediction
-      if (body[linksStrings[i]] == '-1'){
-        var emTag = createStyledEmElement(link.textContent);
-        emTag.addEventListener('click', function (event) {
-          if (!isPopupDisplayed) {
-            // Create a pop-up (an alert) with options to proceed or cancel
-            event.preventDefault(); // Prevent the default link behavior
-            var confirmation = confirm('This is potentially malicious. Click OK to proceed to the link or Cancel to stay on this page.');
-            if (confirmation) {
-              // User clicked OK, allow the link to be followed
-              isPopupDisplayed = false;
-              window.location.href = link.href;
-            } else {
-              // User clicked Cancel, do not follow the link
-              isPopupDisplayed = false;
+  if (body.ok) {
+    for (var i = 0; i < linksObjects.length; i++) {
+      var link = linksObjects[i];
+      if (linksStrings[i] in body){
+        // -1 refers to a malicous prediction
+        if (body[linksStrings[i]] == '-1'){
+          var emTag = createStyledEmElement(link.textContent);
+          emTag.addEventListener('click', function (event) {
+            if (!isPopupDisplayed) {
+              // Create a pop-up (an alert) with options to proceed or cancel
+              event.preventDefault(); // Prevent the default link behavior
+              var confirmation = confirm('This is potentially malicious. Click OK to proceed to the link or Cancel to stay on this page.');
+              if (confirmation) {
+                // User clicked OK, allow the link to be followed
+                isPopupDisplayed = false;
+                window.location.href = link.href;
+              } else {
+                // User clicked Cancel, do not follow the link
+                isPopupDisplayed = false;
+              }
             }
-          }
-        });
-        link.innerHTML = '';
-        link.appendChild(emTag);
+          });
+          link.innerHTML = '';
+          link.appendChild(emTag);
+        }
       }
     }
   }
