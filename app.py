9d5417fb6af93ac8204dd1ad99a4bb866fd73528
ritalamykin <ritalamykin1@gmail.com>
2023-09-09 13:30:51 +0300

    commit

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ import os
 warnings.filterwarnings('ignore')
 from feature import FeatureExtraction
 
-file = open("phishing-url-analyzer/pickle/model.pkl","rb")
+file = open("pickle/model.pkl","rb")
 gbc = pickle.load(file)
 file.close()
 
