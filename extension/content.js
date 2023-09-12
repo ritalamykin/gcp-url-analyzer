e204496b5da4ee546caa834e29e952d1005767fd
ritalamykin <ritalamykin1@gmail.com>
2023-09-13 01:58:46 +0300

    fix issues with clicks

--- extension/content.js
+++ extension/content.js
@@ -1,40 +1,44 @@
 const SERVER_URL = "https://gcp-url-analyzer-qh7fkw5ema-ew.a.run.app";
 var isPopupDisplayed = false; // Variable to track pop-up display status
+var isFirstPopupConfirmed = false; // Variable to track the first popup confirmation
 
 async function markLinks() {
   var linksObjects = document.getElementsByTagName('a');
   var linksStrings = [].slice.call(linksObjects).map(function (a) {
-      return a.href;
-    });
-  console.log("Sending request to server")
+    return a.href;
+  });
+  console.log("Sending request to server");
   const response = await fetch(SERVER_URL, {
-      method: "POST", 
-      cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-      redirect: "follow", // manual, *follow, error
-      referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-      body: JSON.stringify({ "urls": linksStrings}), // body data type must match "Content-Type" header
+    method: "POST",
+    cache: "no-cache",
+    redirect: "follow",
+    referrerPolicy: "no-referrer",
+    body: JSON.stringify({ "urls": linksStrings }),
   });
   var body = await response.json();
-  console.log(body)
+  console.log(body);
   for (var i = 0; i < linksObjects.length; i++) {
     var link = linksObjects[i];
-    if (linksStrings[i] in body){
-      // -1 refers to a malicous prediction
-      if (body[linksStrings[i]] == '-1'){
-        var emTag = createStyledEmElement(link.textContent);
+    if (linksStrings[i] in body) {
+      if (body[linksStrings[i]] == '1') {
+        var emTag = createStyledEmElement(link.textContent, link.href);
         emTag.addEventListener('click', function (event) {
           if (!isPopupDisplayed) {
-            // Create a pop-up (an alert) with options to proceed or cancel
-            event.preventDefault(); // Prevent the default link behavior
+            event.preventDefault();
             var confirmation = confirm('This is potentially malicious. Click OK to proceed to the link or Cancel to stay on this page.');
             if (confirmation) {
-              // User clicked OK, allow the link to be followed
               isPopupDisplayed = false;
-              window.location.href = link.href;
+              isFirstPopupConfirmed = true;
+              var secondConfirmation = confirm('Are you sure you would like to continue?');
+              if (secondConfirmation) {
+                window.location.href = event.currentTarget.getAttribute('data-href');
+              }
             } else {
-              // User clicked Cancel, do not follow the link
               isPopupDisplayed = false;
+              isFirstPopupConfirmed = false;
             }
+          } else if (!isFirstPopupConfirmed) {
+            event.preventDefault(); // Prevent navigation if the first popup was not confirmed
           }
         });
         link.innerHTML = '';
@@ -44,10 +48,11 @@ async function markLinks() {
   }
 }
 
-function createStyledEmElement(text) {
+function createStyledEmElement(text, linkHref) {
   var emTag = document.createElement('em');
   emTag.textContent = text;
   applyStyles(emTag);
+  emTag.setAttribute('data-href', linkHref);
   return emTag;
 }
 
@@ -57,7 +62,7 @@ function applyStyles(element) {
   element.style.backgroundColor = '#ffc6b3';
   element.style.color = 'red';
   element.style.borderRadius = '5px';
-  element.style.fontFamily = 'Arial, sans-serif';
+  element.style.fontFamily = 'Arial, sans-serif'; 
   element.style.fontWeight = 'bold';
   element.style.boxShadow = '2px 2px 5px rgba(0, 0, 0, 0.2)';
   element.style.cursor = 'pointer';
@@ -65,4 +70,3 @@ function applyStyles(element) {
 
 // Run the function when the page finishes loading
 window.addEventListener('load', markLinks);
-
