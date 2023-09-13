52b2c094de7952219711a278015fa4eb6e2a4c5e
ritalamykin <ritalamykin1@gmail.com>
2023-09-13 15:53:02 +0300

    hi

--- extension/content.js
+++ extension/content.js
@@ -1,3 +1,4 @@
+
 const SERVER_URL = "https://gcp-url-analyzer-qh7fkw5ema-ew.a.run.app";
 var isPopupDisplayed = false; // Variable to track pop-up display status
 var isFirstPopupConfirmed = false; // Variable to track the first popup confirmation
@@ -5,9 +6,9 @@ var isFirstPopupConfirmed = false; // Variable to track the first popup confirma
 async function markLinks() {
   var linksObjects = document.getElementsByTagName('a');
   var linksStrings = [].slice.call(linksObjects).map(function (a) {
-      return a.href;
-    });
-  console.log("URLAdvisor: Sent request to server")
+    return a.href;
+  });
+  console.log("URLAdvisor: Sent request to server");
   const response = await fetch(SERVER_URL, {
     method: "POST",
     cache: "no-cache",
@@ -16,32 +17,14 @@ async function markLinks() {
     body: JSON.stringify({ "urls": linksStrings }),
   });
   var body = await response.json();
-  console.log("URLAdvisor: Recieved response from server")
+  console.log("URLAdvisor: Received response from server");
+
 
   for (var i = 0; i < linksObjects.length; i++) {
     var link = linksObjects[i];
     if (linksStrings[i] in body) {
-      if (body[linksStrings[i]] == '-1') {
+      if (body[linksStrings[i]] == '1') {
         var emTag = createStyledEmElement(link.textContent, link.href);
-        emTag.addEventListener('click', function (event) {
-          if (!isPopupDisplayed) {
-            event.preventDefault();
-            var confirmation = confirm('This is potentially malicious. Click OK to proceed to the link or Cancel to stay on this page.');
-            if (confirmation) {
-              isPopupDisplayed = false;
-              isFirstPopupConfirmed = true;
-              var secondConfirmation = confirm('Are you sure you would like to continue?');
-              if (secondConfirmation) {
-                window.location.href = event.currentTarget.getAttribute('data-href');
-              }
-            } else {
-              isPopupDisplayed = false;
-              isFirstPopupConfirmed = false;
-            }
-          } else if (!isFirstPopupConfirmed) {
-            event.preventDefault(); // Prevent navigation if the first popup was not confirmed
-          }
-        });
         link.innerHTML = '';
         link.appendChild(emTag);
       }
@@ -63,11 +46,26 @@ function applyStyles(element) {
   element.style.backgroundColor = '#ffc6b3';
   element.style.color = 'red';
   element.style.borderRadius = '5px';
-  element.style.fontFamily = 'Arial, sans-serif'; 
+  element.style.fontFamily = 'Arial, sans-serif';
   element.style.fontWeight = 'bold';
   element.style.boxShadow = '2px 2px 5px rgba(0, 0, 0, 0.2)';
   element.style.cursor = 'pointer';
 }
+
+window.addEventListener('click', function (event) {
+  if (!isPopupDisplayed && event.target.tagName === 'EM') {
+    console.log("URLAdvisor: inside!!");
+    event.stopImmediatePropagation()
+    event.preventDefault();
+    var confirmation = confirm('This is potentially malicious. Click OK to proceed to the link or Cancel to stay on this page.');
+    if (confirmation) {
+      var secondConfirmation = confirm('Are you sure you would like to continue?');
+      if (secondConfirmation) {
+        window.location.href = event.target.getAttribute('data-href');
+      }
+    }
+  }
+}, true);
 
 // Run the function when the page finishes loading
 window.addEventListener('load', markLinks);
